5000e1fd1b67999626943fe56cc4378e5e48113d
Sweety Seelam <sweetyseelam2@gmail.com>
2025-04-25 02:18:24 -0400

    ✨ Updated index.html, app.js, and dynamicTitle.js with new project, copyright, branding

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const favicon = document.getElementById('favicon')
   const pageTitle = document.title
-  const attentionMessage = 'Come back'
+  const attentionMessage = 'Please visit again'
 
   document.addEventListener('visibilitychange', function (e) {
     const isPageActive = !document.hidden
